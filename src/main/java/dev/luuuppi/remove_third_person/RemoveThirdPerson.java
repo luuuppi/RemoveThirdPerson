e68e0b861e67cb48fd2f65a19cf477cbcba2cc79
Mikhail <Mikhail.Yakushinale@Gmail.com>
2025-06-25 17:24:25 +0700

    init commit

---
+++ src/main/java/dev/luuuppi/remove_third_person/RemoveThirdPerson.java
@@ -0,0 +1,11 @@
+package dev.luuuppi.remove_third_person;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RemoveThirdPerson.MOD_ID)
+public class RemoveThirdPerson {
+  public static final String MOD_ID = "remove_third_person";
+
+  public RemoveThirdPerson() {
+  }
+}
