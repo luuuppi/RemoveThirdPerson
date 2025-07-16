e7727544a14f17e3d21a51e291bdf5ce04640fee
Mikhail <Mikhail.Yakushinale@Gmail.com>
2025-07-16 13:29:51 +0700

    refactor: adjust mixins for compatibility with Minecraft 1.16.5 API

--- src/main/java/dev/luuuppi/remove_third_person/mixins/OptionsMixin.java
+++ src/main/java/dev/luuuppi/remove_third_person/mixins/OptionsMixin.java
@@ -1,16 +1,16 @@
 package dev.luuuppi.remove_third_person.mixins;
 
-import net.minecraft.client.CameraType;
-import net.minecraft.client.Options;
+import net.minecraft.client.GameSettings;
+import net.minecraft.client.settings.PointOfView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Options.class)
+@Mixin(GameSettings.class)
 public class OptionsMixin {
   @Inject(method = "setCameraType", at = @At("HEAD"), cancellable = true)
-  private void disableManualPerspectiveChange(CameraType pPointOfView, CallbackInfo ci) {
+  private void disableManualPerspectiveChange(PointOfView pPointOfView, CallbackInfo ci) {
     ci.cancel();
   }
 }
